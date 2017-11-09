b094add21e0fcf0a3815537890915bb739745e0d
Midas Scheffers <midasscheffers@gmail.com>
2017-11-09 21:26:05 +0100

    Make screen ready to use

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var xScreenSize = 1200;
-var yScreenSize = 900;
+var xScreenSize = innerWidth-5;
+var yScreenSize = innerHeight-4;
 
 function setup() {
   createCanvas(xScreenSize, yScreenSize);
 }
 
 function draw() {
-  
+  background(0);
+  fill(0, 255, 0);
+  noStroke();
+  rect(xScreenSize/2-20, yScreenSize/2-20, 40, 40);
 }
