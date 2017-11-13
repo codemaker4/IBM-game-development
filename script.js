70a9366403d40ed8eb5fa911aaf99d85187a5368
Midas Scheffers <midasscheffers@gmail.com>
2017-11-13 10:27:57 +0100

    [add] make stage based game

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var xScreenSize = innerWidth-20;
 var yScreenSize = innerHeight-20;
+var stage = 0
 
 function setup() {
   createCanvas(xScreenSize, yScreenSize);
 }
 
 function draw() {
-  background(0);
-  fill(0, 255, 0);
-  noStroke();
-  rect(xScreenSize/2-20, yScreenSize/2-20, 40, 40);
+  if (stage == 0){
+    background(0);
+    fill(0, 255, 0);
+    noStroke();
+    rect(xScreenSize/2-20, yScreenSize/2-20, 40, 40);
+  }
 }
