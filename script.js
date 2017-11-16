aec49aaa342576ce8352a50f840e8efcb61cff93
Midas Scheffers <midasscheffers@gmail.com>
2017-11-16 10:16:01 +0100

    [change] fixxx

--- script.js
+++ script.js
@@ -15,19 +15,41 @@ function wall(X,Y) {
 }
 
 function player() {
-  this.xPos = 0;
-  this.yPos = 0;
+  this.xPos = 100;
+  this.yPos = 100;
   // controls
+  this.controls = function() {
+    if (keyIsDown(65)) { //a
+      this.xPos-=5;
+    }
+    if (keyIsDown(68)) { //d
+      this.xPos+=5;
+    }
+    if (keyIsDown(87)) { //w
+      this.yPos-=5;
+    }
+    if (keyIsDown(83)) { //s
+      this.yPos+=5;
+    }
+  }
   // hitboxing walls
   // hitboxing bullets
   // hitboxing enemys
   // render
+  this.render = function() {
+    fill(0,0,0); //color = black
+    ellipse(this.xPos,this.yPos,5,5)
+  }
 }
+
+var Player = new player();
 
 function draw() {
   if (stage == 0){
-    background(0);
+    background(100);
     fill(0, 255, 0);
     noStroke();
+    Player.controls();
+    Player.render();
   }
 }
